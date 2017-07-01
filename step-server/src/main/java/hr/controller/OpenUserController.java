3bafb7093e1234b3cfa9c6e206f52194c6bea9a1
zgy_mail <zgy_mail@126.com>
2017-07-01 14:29:48 +0800

    dddff

--- step-server/src/main/java/hr/controller/OpenUserController.java
+++ step-server/src/main/java/hr/controller/OpenUserController.java
@@ -33,7 +33,6 @@ import io.swagger.annotations.ApiResponses;
 @RequestMapping(value="/open/user",consumes={MediaTypeExt.APPLICATION_PROTOBUF_VALUE,MediaTypeExt.APPLICATION_JSON_UTF8_VALUE},produces={MediaTypeExt.APPLICATION_PROTOBUF_VALUE,MediaTypeExt.APPLICATION_JSON_UTF8_VALUE})
 
 public class OpenUserController {
-
 	 private static final Logger logger = LoggerFactory.getLogger(OpenUserController.class.getName());
 
     @Autowired
